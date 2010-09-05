d0b33cd0fa457931af2d372811edd8b437f784be
Robert Boone <robo4288@gmail.com>
2010-09-05 17:05:10 -0500

    Initial Commit

---
+++ src/small_jobs/core.clj
@@ -0,0 +1,1 @@
+(ns small-jobs.core)
---
+++ test/small_jobs/test/core.clj
@@ -0,0 +1,6 @@
+(ns small-jobs.test.core
+  (:use [small-jobs.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
