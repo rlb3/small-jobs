de0590377df4c76ea50fbc15562e93399c1aa2e2
Robert Boone <robo4288@gmail.com>
2010-09-05 17:12:27 -0500

    Update project.clj
    Added dependencies
        congomongo
        compojure
        ring/ring

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject small-jobs "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Small Jobs Project"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [congomongo "0.1.3-SNAPSHOT"]
+                 [compojure "0.4.1"]
+                 [ring/ring "0.2.5"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
