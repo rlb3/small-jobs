f72678c039323718b47672b7d88ba7606c08a5b9
Robert Boone <robo4288@gmail.com>
2010-09-05 18:29:17 -0500

    Find user by username and email

--- src/small_jobs/users.clj
+++ src/small_jobs/users.clj
@@ -1,5 +1,14 @@
-(ns small-jobs.users)
+(ns small-jobs.users
+  (:require [small-jobs.db :as db]))
 
 (defn create-user [username email skills friends groups temp-groups]
-  {:username username :email email :skills skills :friends friends :groups groups :temp-groups temp-groups})
+  (db/insert-user {:username username :email email :skills skills :friends friends :groups groups :temp-groups temp-groups}))
 
+(defn find-user-by-username [username]
+  (db/find-user-by-username username))
+
+(defn find-user-by-email [email]
+  (db/find-user-by-email email))
+
+(defn update-user-friends [user friends]
+  (db/update-user-friends user friends))
