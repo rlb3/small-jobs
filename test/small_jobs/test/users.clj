aa67614f27031e38a66885fb2d47ed29f5a81429
Robert Boone <robo4288@gmail.com>
2010-09-06 00:42:35 -0500

    Clean up

--- test/small_jobs/test/users.clj
+++ test/small_jobs/test/users.clj
@@ -4,17 +4,22 @@
   (:use [somnium.congomongo])
   (:require [small-jobs.db :as db]))
 
+(def database-name "testsj")
 
+(defn setup! []
+  (db/connect database-name)
+  ;; [username email skills friends groups temp-groups]
+  (create-user "rlb3" "robert@example.com" [:trombone :programming] [] [] [])
+  (create-user "jt" "jt@example.com" [:management :lifecoach] [] [] []))
+
+(defn teardown! []
+  (drop-database! database-name))
 
 (defmacro with-db [& body]
   `(do
-     (db/connect "testsj")
-     ;; [username email skills friends groups temp-groups]
-     (create-user "rlb3" "robert@example.com" [:trombone :programming] [] [] [])
-     (create-user "jt" "jt@example.com" [:management :lifecoach] [] [] [])
+     (setup!)
      ~@body
-     (drop-database! "testsj")))
-
+     (teardown!)))
 
 (deftest test-find-user-by-username
   (with-db
