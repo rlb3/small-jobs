52edb0d81eb149f2338bb4393f79d2f8f32ed479
Robert Boone <robo4288@gmail.com>
2010-09-05 21:28:51 -0500

    Add some docs

--- src/small_jobs/db.clj
+++ src/small_jobs/db.clj
@@ -1,28 +1,40 @@
 (ns small-jobs.db
   (:require [somnium.congomongo :as db]))
 
-(defn connect [db]
+(defn connect
+  "Connect to database"
+  [db]
   (db/mongo!
    :host "127.0.0.1"
    :db db))
 
-(defn id [obj]
+(defn id
+  "Retrieve id from user"
+  [obj]
   (.toString (:_id obj)))
 
 ;; User functions
-(defn insert-user [user]
+(defn insert-user
+  "Insert user into the database"
+  [user]
   (db/insert! :users user))
 
-(defn find-user-by-username [username]
+(defn find-user-by-username
+  "Search for user"
+  [username]
   (db/fetch-one :users :where {:username username}))
 
 (defn find-user-by-email [email]
   (db/fetch-one :users :where {:email email}))
 
-(defn find-user-by-id [id-str]
+(defn find-user-by-id
+  "Search for user"
+  [id-str]
   (filter (fn [user] (= (id user) id-str)) (db/fetch :users)))
 
-(defn update-user-friends [user friends]
+(defn update-user-friends
+  "Add user ids to their friends list"
+  [user friends]
   (db/update! :users user (merge user {:friends (conj (:friends user) friends)})))
 
 (defn update-user-skills [user skills]
