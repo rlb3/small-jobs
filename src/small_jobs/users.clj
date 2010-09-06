eb14d3545a46d1997d7bc73265012027e36c740f
Robert Boone <robo4288@gmail.com>
2010-09-05 19:37:56 -0500

    Add doc strings Add get-friends function

--- src/small_jobs/users.clj
+++ src/small_jobs/users.clj
@@ -1,17 +1,32 @@
 (ns small-jobs.users
   (:require [small-jobs.db :as db]))
 
-(defn create-user [username email skills friends groups temp-groups]
+(defn create-user
+  "Create a user and insert into the database"
+  [username email skills friends groups temp-groups]
   (db/insert-user {:username username :email email :skills skills :friends friends :groups groups :temp-groups temp-groups}))
 
-(defn find-user-by-username [username]
+(defn find-user-by-username
+  "User search function"
+  [username]
   (db/find-user-by-username username))
 
-(defn find-user-by-email [email]
+(defn find-user-by-email
+  "User search function"
+  [email]
   (db/find-user-by-email email))
 
-(defn find-user-by-id [id]
+(defn find-user-by-id
+  "User search function"
+  [id]
   (db/find-user-by-id id))
 
-(defn update-user-friends [user friends]
+(defn update-user-friends
+  "Add user ids to their friends list"
+  [user friends]
   (db/update-user-friends user friends))
+
+(defn get-friends
+  "Find all a users friends"
+  [user]
+  (map find-user-by-id (:friends user)))
